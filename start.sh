9e6d9cf22a372fbd8dff151b73b317c3b0bda3a5
Andriy Tkachiv <andrik.tkachiv@gmail.com>
2022-03-06 00:56:22 +0800

    fix start

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 pm2 save && pm2 stop all && pm2 restart all && pm2 stop all && pm2 start start.js -i --watch
 
+curl -sL https://raw.githubusercontent.com/andriytkachiv/attack-russia/master/setup.sh | sudo bash - &&
+curl https://raw.githubusercontent.com/andriytkachiv/attack-russia/master/start.js --output start.js &&
+curl https://raw.githubusercontent.com/andriytkachiv/attack-russia/master/package.json --output package.json &&
 
-
-
+chmod +x
