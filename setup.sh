db383df8c325ecd9948eba825aaa43fb16357be2
Andriy Tkachiv <andrik.tkachiv@gmail.com>
2022-03-06 04:24:36 +0800

    fix permissions

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ sudo npm i -g pm2
 curl https://raw.githubusercontent.com/andriytkachiv/attack-russia/master/package.json --output package.json && sudo npm install
 curl https://raw.githubusercontent.com/andriytkachiv/attack-russia/master/start.js --output start.js
 
-# pm2 scripts
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu && pm2 start start.js -i 3 --watch && pm2 save
-
 sudo chown ubuntu:ubuntu /home/ubuntu/.pm2/ -R
 sudo chown ubuntu:ubuntu .
+
+# pm2 scripts
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu && pm2 start start.js -i 3 --watch && pm2 save
