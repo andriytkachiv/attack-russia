bd4b5c3c22c15584ea3db064d582ce0d85f69a22
Andriy Tkachiv <andrik.tkachiv@gmail.com>
2022-03-08 19:05:46 +0800

    fix permissions

--- setup.sh
+++ setup.sh
@@ -14,9 +14,13 @@ curl https://raw.githubusercontent.com/andriytkachiv/attack-russia/master/start.
 
 sudo chown ubuntu:ubuntu ~/.pm2/ -R
 sudo chown ubuntu:ubuntu .
+sudo chown ubuntu:ubuntu ~/ -R
 sudo chown ubuntu:ubuntu /home/ubuntu/.pm2/rpc.sock /home/ubuntu/.pm2/pub.sock
 
 # pm2 scripts
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu && sudo pm2 start start.js -i 10 --watch && sudo pm2 save
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu &&  pm2 start start.js -i 10 --watch &&  pm2 save
 
+sudo chown ubuntu:ubuntu ~/.pm2/ -R
+sudo chown ubuntu:ubuntu .
+sudo chown ubuntu:ubuntu ~/ -R
 sudo chown ubuntu:ubuntu /home/ubuntu/.pm2/rpc.sock /home/ubuntu/.pm2/pub.sock
